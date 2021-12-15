e33997a94fe98a65ab465207e4207942b05fd83f
T1M0UT <gohim322@gmail.com>
2021-12-15 13:20:01 +0200

    Update 123131.py

--- 123131.py
+++ 123131.py
@@ -12,5 +12,14 @@ def read_csv(path):
         return dct 
 
 
+def write_csv(path, dct):
+    with open(path) as csvfile:
+        csvwriter = csv.DictWriter(csvfile, fieldnames=["Task", "Deadline"])
+        csvwriter.writeheader()
+        csvwriter.writerows(dct)
+
+
 if __name__ == '__main__':
-    print(read_csv('123.csv'))
+    dct = read_csv('123.csv')
+    print(dct)
+    write_csv("123.csv", dct)
